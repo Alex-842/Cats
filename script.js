779b4e865a8d9cb4c308d5d15f932870a2f3c737
Alex-842 <two92@yandex.ru>
2022-11-21 21:12:37 +0600

    Form

--- script.js
+++ script.js
@@ -55,7 +55,15 @@ const addCat = function() {
 		})
 }
 
+const popupBlock = document.querySelector(".popup-wrapper");
+popupBlock.querySelector(".popup__close").addEventListener("click", function() {
+	popupBlock.classList.remove("active");
+});
+
+
+
+
 document.querySelector("#add").addEventListener("click", function(e) {
 	e.preventDefault();
-	addCat();
+	popupBlock.classList.add("active")
 })
